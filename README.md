024fbeee132b9b3222346845baa5488494673909
Joao Gabriel C. da Cruz <gabriel_cassuriaga@hotmail.com>
2023-09-14 22:01:18 -0300

    Commit inicial

--- README.md
+++
@@ -1,1 +0,0 @@
-# SafeZone-app
---
+++ app/src/main/java/com/example/sazezoneapp/login/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.example.sazezoneapp.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sazezoneapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+
+    
+}
